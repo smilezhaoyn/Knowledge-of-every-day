cf00e775656a90cf6623088735b847ff629a719e
smilezhaoyn <1410098334@qq.com>
2018-12-22 11:56:32 +0800

    12.21小程序

---
+++ 微信小程序/demo/pages/index/index.js
@@ -0,0 +1,103 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrl:[
+      "/images/a.jpg",
+      "/images/b.jpg",
+      "/images/c.jpg"
+    ],
+    items:[
+      { 
+        imgUrl:"/images/a.jpg",
+        title:"会会架构",
+        price:"6"
+      },
+      {
+        imgUrl: "/images/a.jpg",
+        title: "国耀架构",
+        price: "0.1"
+      },
+      {
+        imgUrl: "/images/a.jpg",
+        title: "志康架构",
+        price: "0.001"
+      },
+      {
+        imgUrl: "/images/a.jpg",
+        title: "会会架构",
+        price: "6"
+      },
+      {
+        imgUrl: "/images/a.jpg",
+        title: "国耀架构",
+        price: "0.1"
+      },
+      {
+        imgUrl: "/images/a.jpg",
+        title: "志康架构",
+        price: "0.001"
+      },
+    ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
